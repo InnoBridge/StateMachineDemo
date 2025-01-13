11e301898aef2a2ae52a1c836b9b320b49527e83
yilengyao <yilengyao@gmail.com>
2025-01-13 19:27:42 +0000

    first commit

---
+++ src/main/java/io/github/innobridge/statemachinedemo/Application.java
@@ -0,0 +1,13 @@
+package io.github.innobridge.statemachinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/innobridge/statemachinedemo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.innobridge.statemachinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
