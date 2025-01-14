e047490befb18f580ec26b79b15af2d9c0eec529
Yi leng Yao <yilengyao@gamil.com>
2025-01-14 13:58:36 -0800

    minimial working version of state machine demo

--- src/main/java/io/github/innobridge/statemachinedemo/Application.java
+++ src/main/java/io/github/innobridge/statemachinedemo/Application.java
@@ -2,12 +2,17 @@ package io.github.innobridge.statemachinedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "io.github.innobridge.statemachine.repository")
+@ComponentScan(basePackages = {
+    "io.github.innobridge.statemachinedemo",
+    "io.github.innobridge.statemachine"
+})
 public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
---
+++ src/main/java/io/github/innobridge/statemachinedemo/controller/StateMachineController.java
@@ -0,0 +1,50 @@
+package io.github.innobridge.statemachinedemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import io.github.innobridge.statemachine.service.StateMachineService;
+import io.github.innobridge.statemachine.state.usecases.InitialHelloWorld;
+import io.github.innobridge.statemachine.state.usecases.childinstances.InitialMeal;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+public class StateMachineController {
+
+    @Autowired
+    private StateMachineService stateMachineService;
+
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "hello world";
+    }
+
+    @PostMapping("/create/helloworld")
+    public String createHelloWorld(
+            @RequestBody(required = false) JsonNode input
+    ) {
+        return stateMachineService.createStateMachine(new InitialHelloWorld(), Optional.ofNullable(input), Optional.empty());
+    }
+
+    @PostMapping("/create/meal")
+    public String createMeal(
+            @RequestBody(required = false) JsonNode input
+    ) {
+        return stateMachineService.createStateMachine(new InitialMeal(), Optional.ofNullable(input), Optional.empty());
+    }
+
+    @PostMapping("/process")
+    public String processStateMachine(@RequestParam String instanceId,
+            @RequestBody(required = false) JsonNode input) {
+        return stateMachineService.processStateMachine(instanceId, Optional.ofNullable(input));
+    }
+}
